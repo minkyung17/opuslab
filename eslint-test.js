ebc8018dc69a6fce8e2770b0d87b2eed84f4f216
minkyung <mkim@it.ucla.edu>
2021-07-13 17:44:41 -0700

    git hook test

---
+++ eslint-test.js
@@ -0,0 +1,12 @@
+/**
+ * Not a real file, just for testing eslint.
+ */
+
+function someFunction() {
+    let string1 = "one";
+    let string2 = "two";
+
+    return string1 + string2 + "three";
+}
+
+someFunction();
