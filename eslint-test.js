05b77fa9ad1af9fe3d13f49e8c1cf717852a1e6d
hciwork <mk2328@cornell.edu>
2021-08-04 22:30:01 -0700

    yes bpr; non-admin

--- eslint-test.js
+++ eslint-test.js
@@ -6,7 +6,7 @@ function someFunction() {
     let string1 = "one";
     let string2 = "two";
 
-    return string1 + string2 + "three" // 8/4 10:24p
+    return string1 + string2 + "three" // 8/4 10:29p
 }
 
 someFunction();
