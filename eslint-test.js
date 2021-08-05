786a6ed5bfa019554b4067724ec82ad9997d093c
hciwork <mk2328@cornell.edu>
2021-08-04 17:37:43 -0700

    testing with non-administrator user

--- eslint-test.js
+++ eslint-test.js
@@ -6,7 +6,7 @@ function someFunction() {
     let string1 = "one";
     let string2 = "two";
 
-    return string1 + string2 + 'three'; // 8/4 5:18p
+    return string1 + string2 + 'three'; // 8/4 5:36p
 }
 
 someFunction();
